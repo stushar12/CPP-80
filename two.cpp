31190b161a2fdb0a9374bb78ad8f600091cdd01a
Tushar Saxena <stushar345@gmail.com>
2021-03-04 16:45:24 +0530

    Add files via upload

--- two.cpp
+++ two.cpp
@@ -1,22 +1,65 @@
-int diameterOpt(Node* root, int& height)
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node 
 {
-    int lh = 0, rh = 0;
-    int ldiameter = 0, rdiameter = 0;
- 
-    if (root == NULL) 
-    {
-        height = 0;
-        return 0; 
-    }
-    ldiameter = diameterOpt(root->left, lh);
-    rdiameter = diameterOpt(root->right, rh);
-    height = max(lh, rh) + 1;
- 
-    return max(lh + rh + 1, max(ldiameter, rdiameter));
+	int data; 
+	Node* left;
+	Node* right;
+};
+
+Node* GetNewNode(int data) 
+{
+	Node* newNode = new Node();
+	newNode->data = data;
+	newNode->left = newNode->right = NULL;
+	return newNode;
 }
-int diameter(Node* node)
+
+Node* Insert(Node* root,int data) 
 {
-int res=0;
-int a=diameterOpt(node,res);
-return a;
+	if(root == NULL)                                      // empty tree
+    {                                           
+		root = GetNewNode(data);
+	}
+	
+	else if(data <= root->data)                         // if data to be inserted start lesser, insert inorder left subtree. 
+    {
+		root->left = Insert(root->left,data);
+	}
+	
+	else                                                // else, insert inorder right subtree. 
+    {
+		root->right = Insert(root->right,data);
+	}
+	return root;
+}
+
+int diameter(Node* root,int &res)
+{
+	if(root==NULL)
+	return 0;
+
+	int lh=diameter(root->left,res);
+	int rh=diameter(root->right,res);
+	res=max(res,lh+rh+1);
+	
+	return 1 + max(lh,rh);
+}
+
+
+int main() 
+{
+	Node* root = NULL;  			// Creating an empty tree
+	root = Insert(root,15);	
+	root = Insert(root,10);	
+	root = Insert(root,20);
+	root = Insert(root,8);
+	root = Insert(root,12);
+	root = Insert(root,17);
+	root = Insert(root,25);
+	int res = 0;
+	diameter(root,res);
+	cout<<res;
+
 }
